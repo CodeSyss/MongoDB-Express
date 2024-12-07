43e57de073821d8556c191f1d7e037cc185740ed
Carlos Hernández <carlos-hernandez@correo.unimet.edu.ve>
2024-12-07 15:06:43 -0400

    Database connection

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+//MONGODB_URI = mongodb+srv://carloshernandez:19972005mano@server-mongodb.ezeyh.mongodb.net/NotesAppDB
+const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env; //Variables de entorno
+
+//const NOTES_APP_MONGODB_HOST = process.env.NOTES_APP_MONGODB_HOST;
+//const NOTES_APP_MONGODB_DATABASE = process.env.NOTES_APP_MONGODB_DATABASE;
+
+const MONGODB_URI =  `mongodb+srv://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
+
+// protocol te indica a que servidor se ejecutará en este caso es para mongodb no http p https
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((db) => console.log("Database is connected"))
+  .catch((err) => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+//config un archivo lee .end y asiga una variable de entorno
+
 const app = require("./server");
+require("./database");
 
 app.listen(app.set("port"), () => {
-  console.log("connected to server on port ",app.set("port"));
+  console.log("connected to server on port ", app.set("port"));
 });
